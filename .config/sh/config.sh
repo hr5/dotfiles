48482223d3c9c90050fe86c5b02d86b3faae9faa
hr5 <>
2015-07-08 15:02:14 +0200

    update bashrc

--- .config/sh/config.sh
+++ .config/sh/config.sh
@@ -17,7 +17,7 @@ export PATH=$PATH:$HOME/.gem/ruby/2.2.0/bin
 alias ls='ls --color=auto'
 alias cmake='cmake -G "Unix Makefiles"'
 alias ll='ls -l -h'
-alias tar='tar --format="posix"'
+alias tar='tar --format="pax"'
 alias grep='grep --color=auto'
 alias objdump='objdump -Mintel'
 
